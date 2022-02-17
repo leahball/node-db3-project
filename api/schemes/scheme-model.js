811d87c687ce9910e7cf6948f9b8c93fdaaa0697
iamleahb <leah.ball@gmail.com>
2022-02-16 21:57:41 -0600

    mvp met

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -154,10 +154,11 @@ function add(scheme) {
   /*
     1D- This function creates a new scheme and resolves to _the newly created scheme_.
   */
- return db('schemes').insert(scheme)
- .then(([scheme_id]) => {
-   return db('schemes').where('scheme_id', scheme_id).first()
- })
+  return db("schemes")
+    .insert(scheme)
+    .then(([scheme_id]) => {
+      return db("schemes").where("scheme_id", scheme_id).first();
+    });
 }
 
 function addStep(scheme_id, step) {
@@ -167,6 +168,17 @@ function addStep(scheme_id, step) {
     and resolves to _all the steps_ belonging to the given `scheme_id`,
     including the newly created one.
   */
+  return db("steps")
+    .insert({
+      ...step,
+      scheme_id,
+    })
+    .then(() => {
+      return db("steps")
+        .join("schemes as sc", "sc.scheme_id", "st.scheme_id")
+        .select("step_id", "step_number", "instructions", "scheme_name")
+        .where("sc.scheme_id", scheme_id);
+    });
 }
 
 module.exports = {
